3f9d0794a92620d04180294fa740ac473c716f6b
Vivek kumar <viveksinghssm496@gmail.com>
2022-07-05 08:45:55 +0530

    Added to CreateTable

--- AddressBook.sql
+++ AddressBook.sql
@@ -4,3 +4,17 @@
 create database AddressBookDB
 
 use AddressBookDB;
+
+--UC2 Crreate AddresssBook table
+create table AddressBook(
+FirstName varchar(50) not null,
+LastName varchar(50) not null,
+Address varchar(50) not null,
+City varchar(50) not null,
+State varchar(50) not null,
+Zip int not null,
+Phone Bigint not null,
+Email varchar(50) not null
+);
+
+select * from AddressBook
