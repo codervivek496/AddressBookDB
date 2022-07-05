8a339ae618b18e998f66c13ff4dd80c3a30c40ae
Vivek kumar <viveksinghssm496@gmail.com>
2022-07-05 09:15:00 +0530

    Added to EditContact

--- AddressBook.sql
+++ AddressBook.sql
@@ -25,3 +25,7 @@ insert into AddressBook values
 ('Vivek', 'Kumar', 'gaulakshni', 'Sasaram', 'Bihar', 821115, 7004437837, 'viveksinghssm496@gmail.com'),
 ('Abhishek', 'Kumar', 'Pagwada', 'Jalandhar', 'Punjab', 399392, 9931532761, 'abhishek@gmmail.com'),
 ('Pooja', 'Kumari', 'gomti nagar', 'Lucknnow', 'Utter Pradesh', 923492, 9631007829, 'poojakumari@gmail.com');
+
+--UC4 Edit Contact in AddressBook table
+
+update AddressBook set Phone = 9431845177 where FirstName = 'Pooja';
